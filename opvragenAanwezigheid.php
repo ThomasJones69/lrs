4cdf21a1abfe98b29b66d7566777a27bddf5f57f
ahtec <gerarddoets@gmail.com>
2018-01-10 16:36:07 +0100

    Bezig met opvragen aanwezigheid

--- opvragenAanwezigheid.php
+++ opvragenAanwezigheid.php
@@ -14,11 +14,11 @@
 	
 	while ($row = mysqli_fetch_array($result)) {
 		echo " <div class='leerling' > ";
-		echo "<img id = " . $row['id'] . " src=" . $row['foto'] . " width=142  onclick='aanwezig(this)'></td>";
-		echo "<div id = " . $row['id'] . " src=" . $row['naam'] . " width=142  onclick='aanwezig(this)'></td>";
-		echo "<img id = " . $row['id'] . " src=" . $row['datum'] . " width=142  onclick='aanwezig(this)'></td>";
-		echo "<img id = " . $row['id'] . " src=" . $row['tijd'] . " width=142  onclick='aanwezig(this)'></td>";
-		echo "<img id = " . $row['id'] . " src=" . $row['tijd'] . " width=142  onclick='aanwezig(this)'></td>";
+		echo "<img id = " . $row['id'] . " src=" . $row['foto'] . " width=142  onclick='aanwezig(this)'>";
+		echo "<div id = " . $row['id'] . " src=" . $row['naam'] . " ></div>";
+		echo "<div id = " . $row['id'] . " src=" . $row['datum']. " ></div>";
+		echo "<div id = " . $row['id'] . " src=" . $row['tijd'] . " ></div>";
+		echo "<div id = " . $row['id'] . " src=" . $row['tijd'] . " width=142  onclick='aanwezig(this)'></div>";
 	}
 	echo "</div >";
 	
