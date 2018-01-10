d117c631129e06ada595fedda9e170d2c73fa328
thomasjones69 <thomas.jones69@gmail.com>
2018-01-10 12:01:47 +0100

    CSS file aangemaakt

--- HTMLPage1.php
+++ HTMLPage1.php
@@ -5,66 +5,81 @@ require_once './connection.php';
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
     <head>
-	<style>
+        <link rel="stylesheet" type="text/css" href="opmaaklrs.css">
+            <style>
 
-.leerling 
-{
-	display:inline-block;
-}	
-</style>
-	
-	
-<script> 
-	
-            function aanwezig(leerling) {
-//                var clickedItem = document.getElementById("IDitem").selectedIndex;
-                console.log(leerling.id);
-                var searchString = document.getElementById("IDitem").selectedIndex;
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-//                        console.log("xhttp.responseText");
-//                        console.log(xhttp.responseText);
-//                        var jsonItemResponse = JSON.parse(xhttp.responseText);
-//                        console.log("jsonItemResponse");
-//                        console.log(jsonItemResponse);
-//                        document.getElementById("item").innerHTML      = jsonItemResponse.item;
-/*                        document.getElementById("item").value = jsonItemResponse.item;
-                        document.getElementById("desc").value = jsonItemResponse.description;
-                        document.getElementById("stock").value = jsonItemResponse.stock;
-                        document.getElementById("minStock").value = jsonItemResponse.minStock;
-                        document.getElementById("maxStock").value = jsonItemResponse.maxStock;
-                        document.getElementById("warehouse").value = jsonItemResponse.warehouse;
-*/                    }
-                };
-                xhttp.open("GET", "registreerAanwezigheid.php?leerlingID=" + searchString, true);
-                xhttp.send();
-            }
+            </style>
 
-	
-</script> 
+
+            <script>
+
+                function aanwezig(leerling) {
+                    //                var clickedItem = document.getElementById("IDitem").selectedIndex;
+                    console.log(leerling.id);
+                    var searchString = document.getElementById("IDitem").selectedIndex;
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                            //                        console.log("xhttp.responseText");
+                            //                        console.log(xhttp.responseText);
+                            //                        var jsonItemResponse = JSON.parse(xhttp.responseText);
+                            //                        console.log("jsonItemResponse");
+                            //                        console.log(jsonItemResponse);
+                            //                        document.getElementById("item").innerHTML      = jsonItemResponse.item;
+                            /*                        document.getElementById("item").value = jsonItemResponse.item;
+                             document.getElementById("desc").value = jsonItemResponse.description;
+                             document.getElementById("stock").value = jsonItemResponse.stock;
+                             document.getElementById("minStock").value = jsonItemResponse.minStock;
+                             document.getElementById("maxStock").value = jsonItemResponse.maxStock;
+                             document.getElementById("warehouse").value = jsonItemResponse.warehouse;
+                             */                    }
+                    };
+                    xhttp.open("GET", "registreerAanwezigheid.php?leerlingID=" + searchString, true);
+                    xhttp.send();
+                }
+
+
+            </script> 
 
 
 
- <meta charset="utf-8" />
-        <title> Leerlingen Registratie Systeem </title>
+            <meta charset="utf-8" />
+            <title> Leerlingen Registratie Systeem </title>
     </head>
     <body>
+        <div class="banner">
+            <header>LRS</header>
+        </div>
+        <nav>
 
-	<?php
-        $sql               = "SELECT * FROM `leerling`";
-        $conn              = connectToDb();
-        $result            = $conn->query($sql);
+        </nav>
+        <div class="klas">
 
 
-        echo "<div class='klas' > ";
+            <?php
+            $sql = "SELECT * FROM `leerling`";
+            $conn = connectToDb();
+            $result = $conn->query($sql);
 
 
-        while ($row = mysqli_fetch_array($result)) {
-			echo " <div class='leerling' > ";
-            echo "<img id = " .$row['id']." src=" . $row['foto'] . " width=142  onclick='aanwezig(this)'></td>";
-        }
-        echo "</div >";
-     ?>
+            echo "<div class='klas' > ";
+
+
+            while ($row = mysqli_fetch_array($result)) {
+                echo " <div class='leerling' > ";
+                echo "<img id = " . $row['id'] . " src=" . $row['foto'] . " width=142  onclick='aanwezig(this)'></td>";
+            }
+            echo "</div >";
+            ?>
+
+
+            <div class="zoek">
+                Dit is test
+            </div>
+
+        </div>
+        <footer>
+            Dit is een footer
+        </footer>
     </body>
 </html>
