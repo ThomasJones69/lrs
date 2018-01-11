5d2ba00b823f8ba3256c07962f8744b32a166d19
ahtec <gerarddoets@gmail.com>
2018-01-11 15:38:46 +0100

    invoeren leerling klaar v6

--- invoerenLeerling.php
+++ invoerenLeerling.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
 require_once './connection.php';
-?>
-
-<html>
-<form>
-	
-
-
-
-<?php
-/*
 $naam 			= 	$_REQUEST['naam'];
 $adres      	=	$_REQUEST['adres'];
 $woonplaats		=	$_REQUEST['woonplaats'];
@@ -19,78 +9,36 @@ $telnood		=	$_REQUEST['telnood'];
 $telouders		=	$_REQUEST['telouders'];
 $foto			=	"fotoos/default.jpg";
 $klas			=	$_REQUEST['klas'];
-$schermvolgnr	=	berekenSchermVolgnr($klas)
+$schermvolgnr	=	berekenSchermVolgnr($klas);
 
-*/
+$conn 	= connectToDb();
+$sql = "SELECT *   FROM `leerling`  where `naam` = '$naam' ";
+$result = $conn->query($sql);
 
-	$naam 	= "aswasdfertwer";
-	$conn 	= connectToDb();
-	$sql = "SELECT *   FROM `leerling`  where `naam` = '$naam' ";
-//	$sql = "SELECT *   FROM `leerling`  ";
-//	echo $sql;
-	$result = $conn->query($sql);
-/*	while ($row = mysqli_fetch_array($result)){
-		echo $row['naam'];
-	
-	}
-	var_dump($result);
-	var_dump($row);
-*/	
-	
-	if ( $result->num_rows  == 0 ) {
-		$klas 			= 1;
-		$schermvolgnr	=	berekenSchermVolgnr($klas);	
-		$foto			=	"fotoos/default.jpg";
-		
-	
-		$sql = "INSERT INTO `leerling`(`naam`, `foto`, `schermvolgnr`, `klas`)" 
-		. "VALUES ('$naam', '$foto' , '$schermvolgnr' , '$klas')";
-		
-	
-		//INSERT INTO `leerling`(`id`, `naam`, `adres`, `woonplaats`, `tel`, `telnood`, `telouders`, `foto`, `schermvolgnr`, `klas`) 
-		// VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])		
-		echo($sql);
-		$conn              = connectToDb();
-		$result            = $conn->query($sql);
+if ( $result->num_rows  == 0 ) {
+	$schermvolgnr	=	berekenSchermVolgnr($klas);	
+	$foto			=	"fotoos/default.jpg";
 
-		
-		}
-	
-	
-	
-	
-
-
-	function berekenSchermVolgnr($par_klas){
-
-	$eruit=1;
+	$sql = "INSERT INTO `leerling`(`naam`, `adres`, `woonplaats`, `tel`, `telnood`, `telouders`, `foto`, `schermvolgnr`, `klas`) "
+		   . "VALUES ( '$naam', '$adres' , '$woonplaats' , '$tel' , '$telnood' , '$telouders' , '$foto','$schermvolgnr','$klas') "; 		
+	echo($sql);
 	$conn              = connectToDb();
-	$par_klas = 3;
-//	echo $par_klas ;
-//	$sql = 
+	$result            = $conn->query($sql);
+	}
+
+
+function berekenSchermVolgnr($par_klas){
+	$conn = connectToDb();
 	$sql = "SELECT MAX(schermvolgnr) AS maxSchermVolgnr FROM leerling where `klas` = ".$par_klas;
-//	echo $sql;
-//	echo "\n";
 	$result = $conn->query($sql);
 	$row = mysqli_fetch_array($result) ;
-//	echo $result->num_rows;
 	if (isset($row['maxSchermVolgnr'])) {
-//		echo $row['maxSchermVolgnr'];
 		$eruit = $row['maxSchermVolgnr'];
-//		echo "\n";
+		$eruit = $eruit + 1;
 	} else	{
 		$eruit = 1;
 	}
-	
-//	echo $eruit;
-//	print_r($eruit);
-			
 	$conn->close();
-//	echo "\n";
-	
-	return($eruit);
-	
-	}
+return($eruit);
+}
 ?>      
-
-</html>
