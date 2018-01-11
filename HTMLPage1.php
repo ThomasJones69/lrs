43fa712da6f10a8ca11a00130451312cae20ee14
thomasjones69 <thomas.jones69@gmail.com>
2018-01-11 15:06:26 +0100

    Verwijzing voor js bestand in htmlpage1

--- HTMLPage1.php
+++ HTMLPage1.php
@@ -5,7 +5,8 @@ require_once './connection.php';
 
 <html>
     <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/lrsscript.js"></script>
 <script> 
 	
             function aanwezig(leerling) {
