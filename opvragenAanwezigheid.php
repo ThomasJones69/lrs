ae7acb6cc7cb17a4baadc22dc9e8b212fadb880c
ahtec <gerarddoets@gmail.com>
2018-01-11 09:35:34 +0100

    Bezig 2 met opvragen aanwezigheid

--- opvragenAanwezigheid.php
+++ opvragenAanwezigheid.php
@@ -1,24 +1,49 @@
 <?php
 	session_start();
 	require_once './connection.php';
-	
+?>
+<html>
+    <head>
+
+<meta charset="utf-8" />
+<title> Leerlingen Registratie Systeem </title>
+</head>
+<body>
+
+
+<link rel="stylesheet" type="text/css" href="opmaaklrs.css">
+<meta charset="utf-8" />
+<title> Leerlingen Registratie Systeem </title>
+
+<div class="banner">
+	<header>LRS</header>
+</div>
+<nav>
+
+</nav>
+
+<div class="klas">
+
+<?php
+
+
 	
 	
 	$sql = "SELECT *   FROM `aanwezigheid` JOIN  `leerling`  on  `leerling`.`id` =  `aanwezigheid`.`leerling_id` " ;
-	echo($sql);
+//	echo($sql);
 	$conn              = connectToDb();
 	$result            = $conn->query($sql);
 	
-	echo "<div class='klas' > ";
 	
 	
 	while ($row = mysqli_fetch_array($result)) {
-		echo " <div class='leerling' > ";
-		echo "<img id = " . $row['id'] . " src=" . $row['foto'] . " width=142  onclick='aanwezig(this)'>";
-		echo "<div id = " . $row['id'] . " src=" . $row['naam'] . " ></div>";
-		echo "<div id = " . $row['id'] . " src=" . $row['datum']. " ></div>";
-		echo "<div id = " . $row['id'] . " src=" . $row['tijd'] . " ></div>";
-		echo "<div id = " . $row['id'] . " src=" . $row['tijd'] . " width=142  onclick='aanwezig(this)'></div>";
+		echo " <div class='leerling' > \n";
+		echo "<img id = " . $row['id'] . " src=" . $row['foto'] . " width=50  ></div> \n";
+		echo "<div class='leerling'>"  . $row['naam'] . " </div>\n";
+		echo "<div class='leerling'>"  . $row['datum']. " </div>\n";
+		echo "<div class='leerling>"   . $row['tijd'] . " </div>\n";
+		echo "<div class='leerling> </div>\n";
+
 	}
 	echo "</div >";
 	
